f0e6f6e9cf203f06947ecda0c6363c3020325e25
Avner Biblarz <abiblarz@csumb.edu>
2017-03-01 22:37:27 +0000

    Updated Main File

--- Project Main File.py
+++ Project Main File.py
@@ -1,19 +1,32 @@
-import argparse
+import numpy as np
 import datetime
-import time
+import numpy
 import cv2
 
 capture = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
+count = 0
 firstFrame = None
 
 while(True):
     # Capture frame-by-frame
     (grabbed, frame) = capture.read()
-    (grabbed2, frame2)=capture.read()
-    text = "Pixels changed: " 
+    (grabbed2, frame2) = capture.read()
+    ret, frame1 = capture.read()
+    diff = frame - frame2
+    avg_color_row = numpy.average(diff, axis = 0)
+    avg_color = numpy.average(avg_color_row, axis = 0)
+    #count +=1
+    #diff = (diff)/(count * 100)
+            
     if not grabbed:
         break
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21,21),0)
+    if not grabbed2:
+        break
+    gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21,21),0)
 
     if firstFrame is None:
@@ -27,21 +40,34 @@ while(True):
     for a in cnts:
         (x,y,w,h) = cv2.boundingRect(a)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
-        text = "Pixels changed: " 
+        cv2.rectangle(frame2, (x,y), (x+w, y+h), (0,255,0), 2)
+        text = avg_color
         
     cv2.putText(frame, "Room Status: {}".format(text), (10,20),
                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0),2)
+    cv2.putText(frame2, "Room Status: {}".format(text), (10,20),
+               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0),2)
     cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-               (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35,
-               (0,0,255),1)
-    
+               (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
+               (0,255,0),1)
+    cv2.putText(frame2, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+               (10, frame2.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
+               (0,255,0),1)
+        
     cv2.imshow("Security Camera", frame)
     cv2.imshow("Threshold", threshold)
     cv2.imshow("Delta Frame", frameDelta)
-    
+    while(capture.isOpened()):
+        if ret == True:
+            out.write(frame1)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            else:
+                break
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
 capture.release()
+out.release()
 cv2.destroyAllWindows()
