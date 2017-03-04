631ce4c564894310441835c9e3f2e38c50da6322
Avner Biblarz <abiblarz@csumb.edu>
2017-03-04 00:45:34 +0000

    Update: Fixed the right pixel output

--- Project Main File.py
+++ Project Main File.py
@@ -6,7 +6,7 @@ import cv2
 detectFace = cv2.CascadeClassifier('C:\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml')
 detectEyes = cv2.CascadeClassifier('C:\\opencv\\build\\etc\\haarcascades\\haarcascade_eye.xml')
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
+out = cv2.VideoWriter('Security Footage.avi', fourcc, 7.0, (640,480))
 num = 0
 x = 0
 y = 0
@@ -19,22 +19,26 @@ while(True):
     (grabbed, frame) = capture.read()
     (grabbed2, frame2) = capture.read()
     ret, frame1 = capture.read()
-    diff = frame - frame2
-    #avg_color_row = numpy.average(diff, axis = 0)
-    #avg_color = numpy.average(avg_color_row, axis = 0)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    frameDelta = cv2.absdiff(gray, gray2)
+    ret, frameDelta = cv2.threshold(frameDelta, 5, 1, cv2.THRESH_BINARY)
+    finalPixel = ((np.sum(frameDelta) / np.size(frameDelta)) * 100)
 
-    newDiff = ((diff[x][y][z]) / 3)
-    newDiff = float("{0:.2f}".format(newDiff))
-    print (newDiff)
+    if finalPixel > 50:
+        out.write(frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
     if not grabbed:
         break
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
     gray = cv2.GaussianBlur(gray, (21,21),0)
     if not grabbed2:
         break
-    gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21,21),0)
-
+   
+    gray2 = cv2.GaussianBlur(gray2, (21,21),0)
+    
     if firstFrame is None:
         firstFrame = gray
         continue
@@ -48,24 +52,23 @@ while(True):
         eyes = detectEyes.detectMultiScale(g)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(color,(ex,ey),(ex+ew, ey+eh),(0,255,0),2)
-    text = newDiff
+       
+    text = ((np.sum(frameDelta) / np.size(frameDelta)) * 100)
+    text = float("{0:.2f}".format(text))
         
-    cv2.putText(frame, "Room Status: {}".format(text), (10,20),
-               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0),2)
-    cv2.putText(frame2, "Room Status: {}".format(text), (10,20),
-               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0),2)
+    cv2.putText(frame, "Pixels Changed: {}".format(text), (10,20),
+               cv2.FONT_HERSHEY_SIMPLEX, 0.6, (124,255,0),2)
+    cv2.putText(frame2, "Pixels Changed: {}".format(text), (10,20),
+               cv2.FONT_HERSHEY_SIMPLEX, 0.6, (124,255,0),2)
     cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-               (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
+               (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 0.75,
                (0,255,0),1)
     cv2.putText(frame2, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-               (10, frame2.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
+               (10, frame2.shape[0] - 10), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 0.75,
                (0,255,0),1)
         
     cv2.imshow("Security Camera", frame)
-    if ret == True:
-        out.write(frame1)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
