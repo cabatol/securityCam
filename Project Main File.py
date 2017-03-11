4663e9247e731ec2bcb2406edff05813053d5067
Avner Biblarz <abiblarz@csumb.edu>
2017-03-11 21:30:43 +0000

    Need Help with Code

--- Project Main File.py
+++ Project Main File.py
@@ -1,13 +1,14 @@
 import numpy as np
 import datetime
+import time
 import numpy
 import cv2
 
 detectFace = cv2.CascadeClassifier('C:\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml')
 detectEyes = cv2.CascadeClassifier('C:\\opencv\\build\\etc\\haarcascades\\haarcascade_eye.xml')
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('Security Footage.avi', fourcc, 7.0, (640,480))
-num = 0
+out = cv2.VideoWriter('Security Footage.avi', fourcc, 5.0, (640,480))
+t0 = time.time()
 x = 0
 y = 0
 z = 0
@@ -25,10 +26,10 @@ while(True):
     ret, frameDelta = cv2.threshold(frameDelta, 5, 1, cv2.THRESH_BINARY)
     finalPixel = ((np.sum(frameDelta) / np.size(frameDelta)) * 100)
 
-    if finalPixel > 50:
-        out.write(frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    if(finalPixel > 25):
+        temp = t0 + 15
+        while(t0 != temp):
+            out.write(frame)
     
     if not grabbed:
         break
@@ -45,7 +46,6 @@ while(True):
     #Face Detection starts here
     faces = detectFace.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
-        num +=1
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0),2)
         g = gray[y: y+h, x: x+w]
         color = frame[y: y+h, x: x+w]
@@ -59,7 +59,7 @@ while(True):
     cv2.putText(frame, "Pixels Changed: {}".format(text), (10,20),
                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (124,255,0),2)
     cv2.putText(frame2, "Pixels Changed: {}".format(text), (10,20),
-               cv2.FONT_HERSHEY_SIMPLEX, 0.6, (124,255,0),2)
+               cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0),2)
     cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 0.75,
                (0,255,0),1)
@@ -71,8 +71,8 @@ while(True):
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 # When everything done, release the capture
 capture.release()
 out.release()
 cv2.destroyAllWindows()
+
